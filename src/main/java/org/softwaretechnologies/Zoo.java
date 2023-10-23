19c5cbcb852aaa2b441c7b987c9eab82d9117f28
ddkryachkova <ddkryachkova@edu.tversu.net>
2023-10-23 09:40:44 +0300

    Дороу

--- src/main/java/org/softwaretechnologies/Zoo.java
+++ src/main/java/org/softwaretechnologies/Zoo.java
@@ -24,23 +24,26 @@ public class Zoo {
      * Кошка: Мурзик
      * Собака: Бобик
      *
-     * Вовращаемый список звуков: moo, woof, meow, woof, meow, woof, moo
+     * Возвращаемый список звуков: moo, woof, meow, woof, meow, woof, moo
      *
      * @return Звуки животных, в алфавитном порядке имени животного.
      */
     public List<String> soundAllAnimalsSortByName() {
         List<String> sounds = new ArrayList<>();
         // TODO заполните корректно список звуков
+
         Comparator <Animal> animaalComparator = new Comparator<Animal>() {
             @Override
             public int compare(Animal animal, Animal t1) {
                 return animal.getName().compareTo(t1.getName());
             }
         };
+
         Collections.sort(animalList, animaalComparator);
         for (Animal a:animalList){
             sounds.add(a.sound());
         }
+
         return sounds;
     }
 
