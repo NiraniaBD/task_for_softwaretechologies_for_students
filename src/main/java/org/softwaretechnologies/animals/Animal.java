44acdf92754cc07dfebf2c3b0089e7fd677be479
ddkryachkova <ddkryachkova@edu.tversu.net>
2023-10-23 09:22:18 +0300

    Дороу

--- src/main/java/org/softwaretechnologies/animals/Animal.java
+++ src/main/java/org/softwaretechnologies/animals/Animal.java
@@ -1,5 +1,8 @@
 package org.softwaretechnologies.animals;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+
 public abstract class Animal {
     private final String name;
 
@@ -19,7 +22,29 @@ public abstract class Animal {
      * @return Метод должен возвращать строку, соответсвующую издаваемого животным звука.
      */
 
+   /* private static class Tester extends Employee {
+        protected Tester(String name, int baseSalary) {
+            super(name, baseSalary);
+        }
 
+        @Override
+        public int getMonthSalary(int month) {
+            return baseSalary * YearMonth.of(LocalDate.now().getYear(), month).lengthOfMonth();
+        }
+    } */
+
+
+    private static class Cat {
+
+    }
+
+    private static class Cow{
+
+    }
+
+    private static class Dog{
+
+    }
 
     public abstract String sound();
 }
