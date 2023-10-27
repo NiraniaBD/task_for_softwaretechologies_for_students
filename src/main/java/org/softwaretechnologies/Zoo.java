596e42850166bcb41cd2754615da28086c94ad3b
ddkryachkova <tver.daria@gmail.com>
2023-10-27 10:48:41 +0300

    Дороу

--- src/main/java/org/softwaretechnologies/Zoo.java
+++ src/main/java/org/softwaretechnologies/Zoo.java
@@ -31,14 +31,9 @@ public class Zoo {
 
     //возвращает список строк, содержащий звуки всех животных, отсортированных по имени
     public List<String> soundAllAnimalsSortByName() {
-        //создается пустой список строк sounds, который б/содержать звуки животных
         List<String> sounds = new ArrayList<>();
         // TODO заполните корректно список звуков
 
-        //создается объект Comparator<Animal> с помощью анонимного класса
-        // который реализует интерфейс Comparator и переопределяет метод compare()
-        // метод compare() сравнивает имена двух животных (полученных в качестве параметров)
-        // и возвращает результат сравнения
         Comparator <Animal> animalComparator = new Comparator<Animal>() {
             @Override
             public int compare(Animal animal, Animal t1) {
@@ -48,10 +43,6 @@ public class Zoo {
 
         //список animalList сортируется с использованием animalComparator
         //по именам животных в порядке возрастания
-
-        //для каждого животного a из отсортированного списка animalList
-        //вызывается метод sound(), который возвращает звук данного животного
-        // полученный звук добавляется в список sounds
         Collections.sort(animalList, animalComparator);
         for (Animal a:animalList){
             sounds.add(a.sound());
